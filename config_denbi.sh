33d331b35cf6a7a95e54f9caf5b412e36f0ec2f7
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-05-08 10:13:57 +0200

    Add Favicon

--- config_denbi.sh
+++ config_denbi.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-export URI_STATION_OFFICE=http://193.196.20.68:6006
-export URI_TRAIN_OFFICE=http://193.196.20.68:6001
-export URI_TRAIN_ROUTER=http://193.196.20.68:6004
+#!/bin/bash       
+
+export URI_STATION_OFFICE=http://193.196.20.86:8763
+export URI_TRAIN_OFFICE=http://193.196.20.86:8762
+export URI_TRAIN_ROUTER=http://193.196.20.86:8764
 export URI_DOCKER_REGISTRY=193.196.20.86
 export DIR_MODELS=/mnt/models
 
